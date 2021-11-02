f0bfb5abfc28fce3e535673e5a8eb25eebdf503b
Elizaveta Koroleva <the.white.nurse@gmail.com>
2021-11-02 14:55:04 +0300

    draft

---
+++ manager.py
@@ -0,0 +1,17 @@
+# data in:
+# add several "in-the-wild" examples 
+# add several complicated "in-the-wild" examples
+# remove bad examples
+# data pairs loading
+# cutting by human-size
+# source masks recieving
+# pairs cutting in bottom/left/right
+# pairs streatching (top-bottom, left-right)
+# pairs rotating
+# pairs resize to 512x512 // result should be: various shapes of person
+
+# data out:
+# resize source - resize costume back
+# insert costume to the source
+# remove green background
+# blend edges of the costume
