467bb90a541bded6c434b695cbc598e6c150043d
Elizaveta Koroleva <the.white.nurse@gmail.com>
2021-11-03 11:11:52 +0300

    pairs loading and resize

--- datain.py
+++ datain.py
@@ -12,11 +12,32 @@ class DataPairingError(Error):
     """When data in pairs doesn't match"""
     pass
 
+class Pair:
+    def __init__(self, source_img, dest_img):
+        self.source_img = source_img
+        self.dest_img = dest_img
+    def show_source(self):
+        plt.imshow(cv2.cvtColor(self.source_img, cv2.COLOR_BGR2RGB))
+        plt.show()
+    def show_dest(self):
+        plt.imshow(cv2.cvtColor(self.dest_img, cv2.COLOR_BGR2RGB))
+        plt.show()
+    def resize_pair(self, size):
+        self.source_img = cv2.resize(self.source_img, size, interpolation = cv2.INTER_AREA)
+        self.dest_img = cv2.resize(self.dest_img, size, interpolation = cv2.INTER_AREA)
+    def show_pair(self):
+        if self.source_img.size != self.dest_img.size:
+            self.resize_pair(size=(512, 512))
+        pair_img = np.concatenate((self.source_img, self.dest_img), axis=1)
+        plt.imshow(cv2.cvtColor(pair_img, cv2.COLOR_BGR2RGB))
+        plt.show()
+
 class DataFolder:
     def __init__(self, path: str):
         self.path = path
         self.source_path = path + "/source/"
         self.dest_path = path + "/dest/"
+        self.imgs = []
     def get_source_names(self):
         self.source = os.listdir(self.source_path)
         return self.source
@@ -32,26 +53,50 @@ class DataFolder:
         if len(self.source) != len(self.dest):
             raise DataPairingError("Amoung of files in data \
                                     folders doesn't match")
-    """                     
+            return False
+        return True           
     def load_pairs(self):
-        for source_name, dest_name in zip(self.source, self.dest):
-    """
+        if self.check_count():
+            for source_name, dest_name in zip(self.source, self.dest):
+                source_img = cv2.imread(self.source_path + source_name, cv2.COLOR_BGR2RGB)
+                dest_img = cv2.imread(self.dest_path + dest_name, cv2.COLOR_BGR2RGB)
+                new_pair = Pair(source_img, dest_img)
+                self.imgs.append(new_pair)
+            print(str(len(self.imgs)) + " pair of images loaded")
+            return True
+        return False
+    def unload_pairs(self):
+        del self.imgs
+        self.imgs = []
+    def resize_all_images(self, size=(512, 512)):
+        if self.imgs != []:
+            for pair in self.imgs:
+                pair.resize_pair(size)
+            print("All pairs were resized successfully")
+            
 
     
 data_path = "datacooking/data"
+
 test_data_folder = DataFolder(data_path)
 test_data_folder.get_source_names()
 test_data_folder.get_dest_names()
 test_data_folder.check_names()
+test_data_folder.check_count()
+test_data_folder.unload_pairs()
+test_data_folder.load_pairs()
+test_data_folder.resize_all_images(size = (1920, 1080))
 
-class Pair:
-    def 
+test_images_pair = test_data_folder.imgs[0]
+#test_images_pair.resize_pair(size=(512, 512))
+test_images_pair.show_pair()
+#test_images_pair.show_source()
+
 
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
 frame = cv2.imread("datacooking/data/source/0049.jpg0040.jpg")
-#frame = cv2.resize(frame, (640, 480))
 gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
 
 boxes, weights = hog.detectMultiScale(frame, winStride=(8,8))
@@ -61,9 +106,5 @@ for (xA, yA, xB, yB) in boxes:
     cv2.rectangle(frame, (xA, yA), (xB, yB), (0, 255, 0), 2)
 
 plt.imshow(frame, cmap='gray')
-
-while(True):
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+plt.show()
 
