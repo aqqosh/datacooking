451f1a14ea6e0feccd5a0a14cbcb97d6987d1662
Elizaveta Koroleva <the.white.nurse@gmail.com>
2021-11-05 20:39:26 +0300

    method names changed

--- datain.py
+++ datain.py
@@ -17,22 +17,22 @@ class Pair:
         self.source_img = source_img
         self.dest_img = dest_img
         self.param = param
-    def show_source(self):
+    def ShowSource(self):
         plt.imshow(cv2.cvtColor(self.source_img, cv2.COLOR_BGR2RGB))
         plt.show()
-    def show_dest(self):
+    def ShowDest(self):
         plt.imshow(cv2.cvtColor(self.dest_img, cv2.COLOR_BGR2RGB))
         plt.show()
-    def resize_pair(self, size):
+    def ResizePair(self, size):
         self.source_img = cv2.resize(self.source_img, size, interpolation = cv2.INTER_AREA)
         self.dest_img = cv2.resize(self.dest_img, size, interpolation = cv2.INTER_AREA)
-    def show_pair(self):
+    def ShowPair(self):
         if self.source_img.size != self.dest_img.size:
-            self.resize_pair(size=(512, 512))
+            self.ResizePair(size=(512, 512))
         pair_img = np.concatenate((self.source_img, self.dest_img), axis=1)
         plt.imshow(cv2.cvtColor(pair_img, cv2.COLOR_BGR2RGB))
         plt.show()
-    def detect_human(self):
+    def DetectHuman(self):
         hog = cv2.HOGDescriptor()
         hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
         #gray = cv2.cvtColor(self.source_img, cv2.COLOR_RGB2GRAY)
@@ -42,12 +42,12 @@ class Pair:
             cv2.rectangle(self.source_img, (xA, yA), (xB, yB), (0, 255, 0), 2)
         for (xA, yA, xB, yB) in self.boxes:
             cv2.rectangle(self.dest_img, (xA, yA), (xB, yB), (0, 255, 0), 2)
-    def crop(self):
+    def Crop(self):
         human = self.boxes[0]
         xA, yA, xB, yB = human
         self.source_img = self.source_img[yA:yB, xA:xB]
         self.dest_img = self.dest_img[yA:yB, xA:xB]
-    def transformation(self, transform, execute_all=False):
+    def Transformation(self, transform, execute_all=False):
         if execute_all:
             transformed = transform(image=self.source_img, mask=self.dest_img)
             return Pair(transformed['image'], 
@@ -67,25 +67,25 @@ class DataFolder:
         self.source_path = path + "/source/"
         self.dest_path = path + "/dest/"
         self.imgs = []
-    def get_source_names(self):
+    def GetSourceNames(self):
         self.source = os.listdir(self.source_path)
         return self.source
-    def get_dest_names(self):
+    def GetDestNames(self):
         self.dest = os.listdir(self.dest_path)
         return self.dest
-    def check_names(self):
+    def CheckNames(self):
         for source_name, dest_name in zip(self.source, self.dest):
             if source_name != dest_name:
                 return False
             return True
-    def check_count(self):
+    def CheckCount(self):
         if len(self.source) != len(self.dest):
             raise DataPairingError("Amoung of files in data \
                                     folders doesn't match")
             return False
         return True           
-    def load_pairs(self):
-        if self.check_count():
+    def LoadPairs(self):
+        if self.CheckCount():
             for source_name, dest_name in zip(self.source, self.dest):
                 source_img = cv2.imread(self.source_path + source_name, cv2.COLOR_BGR2RGB)
                 dest_img = cv2.imread(self.dest_path + dest_name, cv2.COLOR_BGR2RGB)
@@ -94,25 +94,25 @@ class DataFolder:
             print(str(len(self.imgs)) + " pair of images loaded")
             return True
         return False
-    def unload_pairs(self):
+    def UnloadPairs(self):
         del self.imgs
         self.imgs = []
-    def resize_all_images(self, size=(512, 512)):
+    def ResizeAllImages(self, size=(512, 512)):
         if self.imgs != []:
             for pair in self.imgs:
-                pair.resize_pair(size)
+                pair.ResizePair(size)
             print("All pairs were resized successfully")
-    def detect_all_humans(self):
+    def DetectAllHumans(self):
         if self.imgs != []:
             for pair in self.imgs:
-                pair.detect_human()
+                pair.DetectHuman()
             print("All humans were found successfully")
-    def crop_all_pairs(self):
+    def CropAllPairs(self):
         if self.imgs != []:
             for pair in self.imgs:
-                pair.crop()
-            print("All crops were done successfully")
-    def save(self, path, type):
+                pair.Crop()
+            print("All Crops were done successfully")
+    def Save(self, path, type):
         if not os.path.exists(path):
             os.mkdir(path) 
         if type == "source":
@@ -120,9 +120,9 @@ class DataFolder:
                 cv2.imwrite(path + name, pair.source_img)
         else:
             print("Types except of source doesn't support")
-    def massive_transform(self, transform, execute_all=False):
+    def MassiveTransform(self, transform, execute_all=False):
         for pair in self.imgs:
-            new_pair = pair.transformation(transform=transform, 
+            new_pair = pair.Transformation(transform=transform, 
                                         execute_all=execute_all)
             if new_pair != None:
                 self.imgs.append(new_pair)
@@ -131,27 +131,27 @@ class DataFolder:
 data_path = "datacooking/data"
 
 test_data_folder = DataFolder(data_path)
-test_data_folder.get_source_names()
-test_data_folder.get_dest_names()
-test_data_folder.check_names()
-test_data_folder.check_count()
-test_data_folder.unload_pairs()
-test_data_folder.load_pairs()
-test_data_folder.resize_all_images(size = (1920, 1080))
+test_data_folder.GetSourceNames()
+test_data_folder.GetDestNames()
+test_data_folder.CheckNames()
+test_data_folder.CheckCount()
+test_data_folder.UnloadPairs()
+test_data_folder.LoadPairs()
+test_data_folder.ResizeAllImages(size = (1920, 1080))
 
 #test_images_pair = test_data_folder.imgs[0]
-#test_images_pair.detect_human()
-#test_images_pair.crop()
-#test_images_pair.show_pair()
+#test_images_pair.DetectHuman()
+#test_images_pair.Crop()
+#test_images_pair.ShowPair()
 
-test_data_folder.detect_all_humans()
-test_data_folder.crop_all_pairs()
-#test_data_folder.save(path="data_cooking/data/crop_source/", type="source")
+test_data_folder.DetectAllHumans()
+test_data_folder.CropAllPairs()
+#test_data_folder.Save(path="data_cooking/data/Crop_source/", type="source")
 
 transform = A.Compose([
     A.HorizontalFlip(p=1),
     A.RandomBrightnessContrast(p=0.2),
 ])
 
-test_data_folder.massive_transform(transform=transform, execute_all=False)
-test_data_folder.imgs[-3].show_pair()
+test_data_folder.MassiveTransform(transform=transform, execute_all=False)
+test_data_folder.imgs[-3].ShowPair()
